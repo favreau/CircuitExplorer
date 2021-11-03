4d2f29ce14d0c90636bbb3777d0e750ee331786b
Cyrille Favreau <cyrille.favreau@epfl.ch>
2021-11-03 11:50:54 +0100

    Logging standardization

--- core/common/Logs.h
+++ core/common/Logs.h
@@ -19,58 +19,51 @@
 #pragma once
 
 #include <iostream>
-#include <thread>
 
 namespace circuitexplorer
 {
 #define PLUGIN_PREFIX "CE"
 
-#define PLUGIN_ERROR(message)                                             \
-    std::cerr << std::dec << "[" << std::this_thread::get_id() << "] E [" \
-              << PLUGIN_PREFIX << "] " << message << std::endl;
-#define PLUGIN_WARN(message)                                              \
-    std::cerr << std::dec << "[" << std::this_thread::get_id() << "] W [" \
-              << PLUGIN_PREFIX << "] " << message << std::endl;
-#define PLUGIN_INFO(message)                                              \
-    std::cout << std::dec << "[" << std::this_thread::get_id() << "] I [" \
-              << PLUGIN_PREFIX << "] " << message << std::endl;
+#define PLUGIN_ERROR(__msg) \
+    std::cerr << "E [" << PLUGIN_PREFIX << "] " << __msg << std::endl;
+#define PLUGIN_WARN(__msg) \
+    std::cerr << "W [" << PLUGIN_PREFIX << "] " << __msg << std::endl;
+#define PLUGIN_INFO(__msg) \
+    std::cout << "I [" << PLUGIN_PREFIX << "] " << __msg << std::endl;
 #ifdef NDEBUG
-#define PLUGIN_DEBUG(message) ;
+#define PLUGIN_DEBUG(__msg) ;
 #else
-#define PLUGIN_DEBUG(message)                                             \
-    std::cout << std::dec << "[" << std::this_thread::get_id() << "] D [" \
-              << PLUGIN_PREFIX << "] " << message << std::endl;
+#define PLUGIN_DEBUG(__msg) \
+    std::cout << "D [" << PLUGIN_PREFIX << "] " << __msg << std::endl;
 #endif
-#define PLUGIN_TIMER(__time, __msg)                                       \
-    std::cout << std::dec << "[" << std::this_thread::get_id() << "] T [" \
-              << PLUGIN_PREFIX << "] " << __msg << " in " << __time       \
+#define PLUGIN_TIMER(__time, __msg)                                          \
+    std::cout << "T [" << PLUGIN_PREFIX << "] " << __msg << " in " << __time \
               << " seconds" << std::endl;
 
-#define PLUGIN_THROW(message)              \
-    {                                      \
-        PLUGIN_ERROR(message);             \
-        throw std::runtime_error(message); \
+#define PLUGIN_THROW(__msg)              \
+    {                                    \
+        PLUGIN_ERROR(__msg);             \
+        throw std::runtime_error(__msg); \
     }
 
-#define PLUGIN_PROGRESS(message, progress, barWidth)                          \
-    {                                                                         \
-        std::cout << std::dec << "[" << std::this_thread::get_id() << "] I [" \
-                  << PLUGIN_PREFIX << "] [";                                  \
-        const float __maxValue = float(barWidth);                             \
-        const float __step = 100.f / __maxValue;                              \
-        const uint32_t __pos = progress / __maxValue * 50.f;                  \
-        for (uint32_t __i = 0; __i < 50; ++__i)                               \
-        {                                                                     \
-            if (__i < __pos)                                                  \
-                std::cout << "=";                                             \
-            else if (__i == __pos)                                            \
-                std::cout << ">";                                             \
-            else                                                              \
-                std::cout << " ";                                             \
-        }                                                                     \
-        std::cout << "] " << std::min(__pos * 2, uint32_t(100)) << "% "       \
-                  << message << "\r";                                         \
-        std::cout.flush();                                                    \
+#define PLUGIN_PROGRESS(__msg, __progress, __maxValue)                  \
+    {                                                                   \
+        std::cout << "I [" << PLUGIN_PREFIX << "] [";                   \
+        const float __mv = float(__maxValue);                           \
+        const float __step = 100.f / __mv;                              \
+        const uint32_t __pos = __progress / __mv * 50.f;                \
+        for (uint32_t __i = 0; __i < 50; ++__i)                         \
+        {                                                               \
+            if (__i < __pos)                                            \
+                std::cout << "=";                                       \
+            else if (__i == __pos)                                      \
+                std::cout << ">";                                       \
+            else                                                        \
+                std::cout << " ";                                       \
+        }                                                               \
+        std::cout << "] " << std::min(__pos * 2, uint32_t(100)) << "% " \
+                  << __msg << "\r";                                     \
+        std::cout.flush();                                              \
     }
 
 } // namespace circuitexplorer
